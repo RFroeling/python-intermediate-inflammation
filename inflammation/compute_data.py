5fa5b44559bb5f7e077922fc6d7cae090f1abbf3
Rik Froeling <rik.froeling@wur.nl>
2025-11-25 13:00:43 +0100

    Refactors

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -55,6 +55,15 @@ def load_inflammation_data(dir_path: str) -> list[np.ndarray]:
     return list(data)
 
 
+def compute_standard_deviation_by_day(data):
+    means_by_day = map(models.daily_mean, data)
+    means_by_day_matrix = np.stack(list(means_by_day))
+
+    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
+
+    return daily_standard_deviation
+
+
 def analyse_data(data_dir):
     """Calculates the standard deviation by day between datasets.
     Gets all the inflammation data from CSV files within a directory, works out the mean
@@ -70,13 +79,4 @@ def analyse_data(data_dir):
         'standard deviation by day': daily_standard_deviation,
     }
     # views.visualize(graph_data)
-    return daily_standard_deviation
-
-
-    def compute_standard_deviation_by_day(data):
-        means_by_day = map(models.daily_mean, data)
-        means_by_day_matrix = np.stack(list(means_by_day))
-
-        daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
-
     return daily_standard_deviation
